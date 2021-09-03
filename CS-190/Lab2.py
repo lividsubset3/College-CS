63a7fc07dc51be560c5f332ab8b91fd4d77bacb0
Pat_SmallMC <lividsubset3@gmail.com>
2021-09-03 09:08:54 -0600

    Notes for Lab2.py

--- CS-190/Lab2.py
+++ CS-190/Lab2.py
@@ -1,12 +1,15 @@
 # L2 Space Flight calculator
 # Create a calc that calculates how many years does it take to travel x light years
 
+# Input two variables needed for calc
 speed = int(input("Speed of Craft MPS: "))
 distance = float(input("Light years: "))
 
+# Math for calc
 mpy = speed * 60 * 60 * 24 * float(365.5)
 lightYear = distance * 5878625370000
 
+# End Calc
 calc = lightYear // mpy
 
 print(calc)
