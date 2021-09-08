ab90d9d2bb9702beafff4438147250e2db71538f
Pat_SmallMC <lividsubset3@gmail.com>
2021-09-08 09:50:02 -0600

    HW3 | Lab3

---
+++ CS-190/HW3.py
@@ -0,0 +1,4 @@
+# For this assignment, your task is to implement a Mad Lib program in python.
+# you'll prompt the user for at least five inputs then output a Mad Lib.
+# These words can be strings, integers, or doubles. Try to make me laugh
+
---
+++ CS-190/Lab3.py
@@ -0,0 +1,15 @@
+# Implement simple integer calculator in python
+# INPUT 3 Items | user name (string), x (int), y (int)
+# OUTPUT 5 Items | Message to user
+# result of addition (x+y), Multiplication, exponents, modulo
+
+z, x, y = input("User Name: "), float(input("Num x: ")), float(input("Num y: "))
+
+print()
+print("Hello", z, "hope things are going correctly in life.")
+
+print()
+print(x, "+", y, "=", x + y)
+print(x, "*", y, "=", x * y)
+print(x, "^", y, "=", x ^ y)
+print(x, "%", y, "=", x % y)
