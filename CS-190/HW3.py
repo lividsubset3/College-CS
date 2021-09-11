252a8f79947654d5a48708c6e7b6e1fde204a40c
Patrick Rakowicz <lividsubset3@gmail.com>
2021-09-10 18:14:36 -0600

    Complete

--- CS-190/HW3.py
+++ CS-190/HW3.py
@@ -2,3 +2,12 @@
 # you'll prompt the user for at least five inputs then output a Mad Lib.
 # These words can be strings, integers, or doubles. Try to make me laugh
 
+print("Mad Lib Creation...")
+adjective = input("Input adjective: ")
+noun = input("Input noun: ")
+verb = input("Input verb: ")
+noun2 = input("Input noun2: ")
+num = input("Input #: ")
+
+print("6 AM: Wake up to the sound of", noun, "beeping.", "Getting up, i get", num, noun2, "wakes me right up. "
+"Feeling", adjective, "i start my wonderful day.")
