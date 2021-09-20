d6aea9fa348079a7604080f53620e0b87e1afe10
Pat_SmallMC <lividsubset3@gmail.com>
2021-09-20 09:41:51 -0600

    Start Lab5, instructions posted

---
+++ CS-190/Lab5.py
@@ -0,0 +1,10 @@
+# Write Python code that outputs the lyrics to "99 Bottles of beer"
+# verses:
+# x bottles of beer on the wall | X bottles of beer (take one down, pass it around)
+# X-1 bottles of beer on the wall
+# Bottle of beer / Bottles of beer
+# Ends - no more bottles of beer on the wall, no more bottles of beer
+# - go to the store, by some more, 99 bottles of beer on the wall
+
+
+
