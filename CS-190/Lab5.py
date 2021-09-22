b6e428ccb745197532008a74c38cdc3af1ea0ca9
Pat_SmallMC <lividsubset3@gmail.com>
2021-09-22 09:38:20 -0600

    For Loops | Done

--- CS-190/Lab5.py
+++ CS-190/Lab5.py
@@ -6,4 +6,19 @@
 # Ends - no more bottles of beer on the wall, no more bottles of beer
 # - go to the store, by some more, 99 bottles of beer on the wall
 
+b = 12
+
+if b >= 1:
+    for i in range(12):
+        if b == 1:
+            print(str(b), "Bottle of beer on the wall...")
+            print(str(b), "Bottle of beer...")
+            print("Take one down, pass it around...")
+            print("No more bottles of beer on the wall.")
+        else:
+            print(str(b), "Bottles of beer on the wall...")
+            print(str(b), "Bottles of beer...")
+            b -= 1
+print("Go to the store, buy some more")
+print("99 Bottles of beer on the wall")
 
