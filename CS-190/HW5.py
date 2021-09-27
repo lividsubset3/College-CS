0b453389065b182f5b68db92237a6b8e831c7051
Patrick Rakowicz <lividsubset3@gmail.com>
2021-09-26 20:15:18 -0600

    Final state, Needs Debugging

---
+++ CS-190/HW5.py
@@ -0,0 +1,18 @@
+# Multiply using addition without using the multiplication tag
+
+x = int(input("Main Value: "))
+y = int(input("Value to be multiplied: "))
+
+product = y
+z = 1
+
+if x >= 0 and y >= 0:
+    print()
+    for i in range(z, y + 1):
+        print("after integration |", x, "x", i, "=", product)
+        product = product + x
+    print(x, "*", y, "=", product)
+else:
+    print()
+    print("X or Y value is not positive")
+    print("please enter Positive Number..")
