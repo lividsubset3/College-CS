568bbc6fdbcb26b623ccfe99c0f21887636137a7
Patrick Rakowicz <lividsubset3@gmail.com>
2021-10-10 19:51:20 -0600

    Finished | Debugged

--- CS-190/HW7.py
+++ CS-190/HW7.py
@@ -2,28 +2,37 @@ word_list = []
 
 while True:
     value = input("Enter a word ('Quit to exit'): ")
+    word_list.append(value)
 
     if value == 'Quit':
-        print("Original")
-        for words in word_list:
-            print(words)
 
-        print()
-        print("Sorted")
-        sorted_string = sorted(word_list)
-        for words in sorted_string:
-            print(words)
+        word_list.remove('Quit')
 
-        print()
+        print("\n")
+        print("Original List")
+        print("===")
+        for i in range(len(word_list)):
+            print(word_list[i], end=' ')
+
+        print("\n")
+        print("Sorted List")
+        print("===")
+        sort_list = sorted(word_list, key=len)
+        for i in range(len(sort_list)):
+            print(sort_list[i], end=' ')
+
+        print("\n")
         print("Reversed")
+        print("===")
+        reverse_list = reversed(word_list)
+        print(*reverse_list)
 
-        print()
+        print("")
         print("Longest Word")
         longest_word = max(word_list, key=len)
-        for words in word_list:
-            print(words)
-        print(longest_word, "Longest word")
+        print(longest_word)
 
         break
-    else value == '':
+
+    elif value == '':
         continue
