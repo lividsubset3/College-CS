2d012b038da5a055bba8b4860ad05628da21baf8
Pat_SmallMC <lividsubset3@gmail.com>
2021-10-11 10:01:04 -0600

    Init

---
+++ CS-190/Lab8.py
@@ -0,0 +1,8 @@
+# write python function to compute:
+# volume of a box (input | length, width, height)
+# volume of a sphere (Given radius, Height)
+# volume of pyramid (Given length, width, height)
+# volume of a cylinder (Given radius, height)
+# Print volume
+
+
---
+++ CS-190/LiveCoding08.py
@@ -0,0 +1,9 @@
+
+def inc(x):
+    x = x + 1
+    print(x)
+
+
+y = 10
+inc(y)
+print(y)
