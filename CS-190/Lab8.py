d218a8f4ad47a63706fafb7dbaaca1d84b10723e
Rako-BMac <patrick.rakowicz@gmail.com>
2021-10-11 23:10:04 -0600

    ++ Functions | Finished | Debugged

--- CS-190/Lab8.py
+++ CS-190/Lab8.py
@@ -7,6 +7,27 @@
 
 import math
 
+
+def volume_box(length, width, height):
+    volume = length * width * height
+    print("Volume:", volume)
+
+
+def volume_sphere(radius, height):
+    volume = (4 / 3) * math.pi * (math.pow(radius, 3)) * height
+    print("Volume:", volume)
+
+
+def volume_pyramid(length, width, height):
+    volume = (length * width * height) / 3
+    print("Volume:", volume)
+
+
+def volume_cylinder(radius, height):
+    volume = math.pi * (math.pow(radius, 2)) * height
+    print("Volume:", volume)
+
+
 while True:
     print("Volume of Box        [1]")
     print("Volume of Sphere     [2]")
@@ -14,39 +35,36 @@ while True:
     print("Volume of Cylinder   [4]")
     print("Quit                 [5]")
     select_menu = input("Select: ")
+    print()
 
     if select_menu == '1':
         print("=== Volume of a Box ===")
-        length = int(input("Input Length: "))
-        width = int(input("Input Width: "))
-        height = int(input("Input Height: "))
-        volume = length * width * height
-        print("Volume:", volume)
+        x = int(input("Input Length: "))
+        y = int(input("Input Width: "))
+        z = int(input("Input Height: "))
+        volume_box(x, y, z)
         print()
 
     if select_menu == '2':
         print("=== Volume of a Sphere ===")
-        radius = int(input("Input Radius: "))
-        height = int(input("Input Height: "))
-        volume = (4/3) * math.pi * (math.pow(radius, 3))
-        print("Volume:", volume)
+        x = int(input("Input Radius: "))
+        y = int(input("Input Height: "))
+        volume_sphere(x, y)
         print()
 
     if select_menu == '3':
         print("=== Volume of a Pyramid ===")
-        length = int(input("Input Length: "))
-        width = int(input("Input Width: "))
-        height = int(input("Input Height: "))
-        volume = (length * width * height) / 3
-        print("Volume:", volume)
+        x = int(input("Input Length: "))
+        y = int(input("Input Width: "))
+        z = int(input("Input Height: "))
+        volume_pyramid(x, y, z)
         print()
 
     if select_menu == '4':
         print("=== Volume of a Cylinder ===")
-        radius = int(input("Input Radius: "))
-        height = int(input("Input Height: "))
-        volume = math.pi * (math.pow(radius, 2)) * height
-        print("Volume:", volume)
+        x = int(input("Input Radius: "))
+        y = int(input("Input Height: "))
+        volume_cylinder(x, y)
         print()
 
     if select_menu == '5':
