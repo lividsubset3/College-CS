a8b0ac5b9157c4830ae7fe5a0c4cec7d7b093de2
Pat_SmallMC <lividsubset3@gmail.com>
2021-10-28 19:41:18 -0600

    Bugged

--- CS-190/HW10.py
+++ CS-190/HW10.py
@@ -14,10 +14,6 @@ def rectangle(x, y, width, height, color):
     canvas.create_rectangle(x, y, x + width, y + height, fill=color, outline=color)
 
 
-def square(x, y, width, height, color):
-    canvas.create_rectangle(x, y, x + width, y + height, fill=color, outline=color)
-
-
 def triangle(x, y, size, color):
     canvas.create_polygon(x - size // 2, y, x + size // 2, y, x, y - size // 2, fill=color, outline=color)
 
@@ -27,39 +23,38 @@ def triangle(x, y, size, color):
 
 def go():
     try:
-        init = 'HW10.txt'
         # init = input("Input File: ")
+        init = "HW10.txt"
         with open(init) as f:
             for line in f:
                 L = line.split(' ')
                 for shape_type in L:
                     if shape_type == 'triangle':
-                        print(str(L[4]))
-                #         x = int(L[1])
-                #         y = int(L[2])
-                #         size = int(L[3])
-                #         color = str(L[4])
-                #         triangle(x, y, size, color)
-                #     elif shape_type == 'rectangle':
-                #         x = int(L[1])
-                #         y = int(L[2])
-                #         width = int(L[3])
-                #         height = int(L[4])
-                #         color = str(L[5])
-                #         rectangle(x, y, width, height, color)
-                #     elif shape_type == 'circle':
-                #         x = int(L[1])
-                #         y = int(L[2])
-                #         radius = int(L[3])
-                #         color = str(L[4])
-                #         circle(x, y, radius, color)
-                #     elif shape_type == 'square':
-                #         x = int(L[1])
-                #         y = int(L[2])
-                #         width = int(L[3])
-                #         height = int(L[4])
-                #         color = str(L[5])
-                #         square(x, y, width, height, color)
+                        x = int(L[1])
+                        y = int(L[2])
+                        size = int(L[3])
+                        color = str(L[4])
+                        triangle(x, y, size, color)
+                    elif shape_type == 'rectangle':
+                        x = int(L[1])
+                        y = int(L[2])
+                        width = int(L[3])
+                        height = int(L[4])
+                        color = str(L[5])
+                        rectangle(x, y, width, height, color)
+                    elif shape_type == 'circle':
+                        x = int(L[1])
+                        y = int(L[2])
+                        radius = int(L[3])
+                        color = str(L[4])
+                        circle(x, y, radius, color)
+                    elif shape_type == 'square':
+                        x = int(L[1])
+                        y = int(L[2])
+                        width = int(L[3])
+                        height = int(L[4])
+                        color = f"'{L[5]}'"
+                        rectangle(x, y, width, height, color)
     except EXCEPTION:
         print("Error | 404 | File Not found")
 
