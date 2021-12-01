bdca411515a4529b29d6c7fcbfb744786b6f6d23
Pat_SmallMC <lividsubset3@gmail.com>
2021-12-01 09:50:44 -0700

    Almost done | Bugged

--- CS-190/Labs/Lab14.py
+++ CS-190/Labs/Lab14.py
@@ -1,15 +1,38 @@
 # Implement "Bounce" in Python
 # Animate shape to bounce off N, E, S, W walls
-
+import random  # random.randrange(startRange, endRange)
 from tkinter import *
 
+window_h = 500  # Y
+window_w = 500  # X
+rand = random.randrange(-8, 8)
+
 root = Tk()
-window_h = 500
-window_w = 500
 window = Canvas(root, width=window_w, height=window_h)
 window.pack()
 
 block = window.create_rectangle(0, 0, 50, 50, fill='black')
-window.move(block, 225, 250)
 
+window.move(block, 225, 225)
+y = 5
+x = 5
+
+
+def _WallDetection():
+    global x, y
+    x0, y0, x1, y1 = window.coords(block)
+
+    window.move(block, x, y)
+    if y1 >= 500:
+        y = -4
+    if x1 >= 500:
+        x = -4
+    if y1 <= 0:
+        y = 4
+    if x0 <= 0:
+        x = 4
+    root.after(35, _WallDetection)
+
+
+root.after(35, _WallDetection)
 root.mainloop()
