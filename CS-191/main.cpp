f5100bd84ffff7dee07d2c021a17ad073c1a25bf
Patrick Rakowicz <lividsubset3@gmail.com>
2022-01-23 20:18:48 -0700

    Practice 0 | Finished

--- CS-191/main.cpp
+++ CS-191/main.cpp
@@ -1,6 +1,27 @@
+/*
+    191
+    practice0
+    main.cpp
+    Patrick Rakowicz
+*/
+
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int m1, m2, difference, product, average;
+    double sum, daver;
+
+    cout << "Please input 2 integers:";
+    cin >> m1 >> m2;
+
+    sum = m1 + m2;
+    difference = m1 - m2;
+    product = m1 * m2;
+    average = sum / 2;
+    daver = sum / 2;
+
+    cout << "Sum: " << sum << " Difference: " << difference << " Product: " << product << endl;
+    cout << "Integer average: " << average << endl;
+    cout << "Double average: " << daver << endl;
 }
