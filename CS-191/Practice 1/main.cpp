5003e11393e5f805bde3858cff2a494ef460dfa8
Patrick Rakowicz <lividsubset3@gmail.com>
2022-01-24 19:11:01 -0700

    Parameters needed to be done

--- CS-191/Practice 1/main.cpp
+++ CS-191/Practice 1/main.cpp
@@ -1,8 +1,34 @@
 /*
     191
-    Pratice 1
+    Practice 1
     main.cpp
     Patrick Rakowicz
 */
+#include <iostream>
+using namespace std;
 
 
+int main() {
+    int m1, m2;
+
+    cout << "Input two integers in increasing order:";
+    cin >> m1 >> m2;
+
+    cout << "Generating random integers" << endl;
+    cout << "Expected:" << endl;
+    cout << "10 Random integers in the range [" << m1 << " , " << m2 << "]" << endl;
+
+    cout << "Actual:" << endl;
+    //  TODO
+    //      Randomly generate 10 integers from the two given integers
+
+    cout << "Expected:" << endl;
+    cout << "average value of the integers" << endl;
+    cout << "maximum value of the integers" << endl;
+    cout << "minimum value of the integers" << endl;
+    cout << "Actual:" << endl;
+    //  TODO
+    //      print Average Values
+    //      print Maximum Values
+    //      print Minimum Values
+}
