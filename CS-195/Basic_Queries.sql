c30e11dbcb8444d01d46ef66be217e6bf9b84d6d
Patrick Rakowicz <lividsubset3@gmail.com>
2022-01-27 20:09:04 -0700

    finished

--- CS-195/Basic_Queries.sql
+++ CS-195/Basic_Queries.sql
@@ -1,7 +1,12 @@
+/* 
+   CS 195
+   Patrick Rakowicz | Caleb Domenico
+   Basic Queries
+*/
 USE Create_Tables;
 
 #1 
-select fname from customerContacts;
+show columns from customerContacts;
 
 #2
 select 
@@ -13,11 +18,49 @@ email as 'Email'
 from customerContacts;
 
 #3
-select * from customerContacts
-order by customerContactId;
+select fname, lname from customerContacts order by customerContactId;
 
 #4
 show tables;
 
 #5
+drop table if exists secretInfo;
 
+#6
+select * from customerContacts where customerId = 'SCI2000';
+
+#7
+select * from orders order by customerId, orderDate;
+
+#8
+select * from orders order by customerId asc, orderDate desc; 
+
+#9
+select * from orders where orderDate > 1/23/2006;
+
+#10
+select * from orderLines where quantity >= 10; 
+
+#11
+select * from orderLines where productID = 'MINI' and quantity >= 10;
+
+#12
+select orderId from orders where customerID = 'MSCRUS' order by orderID desc;
+
+#13
+select count(*) from products where weight > 4; 
+
+#14
+select count(*) as 'Not 5.99' from products where weight != 5.99; 
+
+#15
+select count(*) as 'MXLDR Order Count' from orders where customerId = 'MXLDR';
+
+#16
+select count(orderId) as 'Total Orders' from orders;
+
+#17
+select customerId, orderDate from orders order by orderDate desc; 
+
+#18
+select distinct customerId from orders;
