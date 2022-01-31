b177d0fb6d8c1b8b9b1507a4034fd49429dbd81c
Pat Rak <rako@Pats-MacBook-Pro.local>
2022-01-31 14:42:53 -0700

    init + Started

---
+++ CS-191/For_Loop_Practice/main.cpp
@@ -0,0 +1,30 @@
+/*
+    191
+    For Loop Practice
+    main.cpp
+    Patrick Rakowicz
+*/
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    // Loop 1
+    int m1, n1;
+
+    cout << "Generating Numbers from m to n";
+    cout << "Enter m and n:";
+    cin >> m1 >> n1;
+
+    cout << "Expected: Integers from " << m1 << " to " << n1 << endl;
+
+    // Loop 2
+    int m2, n2;
+
+    cout << "Generating Numbers from m to n";
+    cout << "Enter m and n:";
+    cin >> m2 >> n2;
+
+    cout << "Expected: Integers from " << m2 << " to " << n2 << endl;
+}
