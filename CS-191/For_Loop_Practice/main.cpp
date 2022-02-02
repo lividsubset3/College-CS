a91b92c37f247e563c1892677b0d4976444dcfc7
Patrick Rakowicz <lividsubset3@gmail.com>
2022-02-02 12:42:06 -0700

    Finished

--- CS-191/For_Loop_Practice/main.cpp
+++ CS-191/For_Loop_Practice/main.cpp
@@ -5,19 +5,31 @@
     Patrick Rakowicz
 */
 #include <iostream>
-#include <stdlib.h>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
     // Loop 1
-    int m1, n1;
+    int m1, n1, count;
+    float sum = 0;
 
     cout << "Generating Numbers from m to n";
     cout << "Enter m and n:";
     cin >> m1 >> n1;
 
     cout << "Expected: Integers from " << m1 << " to " << n1 << endl;
+    count = n1 - m1 + 1;
+
+    cout << "Actual: ";
+    for (int i = m1; i < (n1 + 1); ++i) {
+        cout << i << " ";
+        sum += i;
+    }
+    cout << endl;
+    cout << setprecision(2) << fixed << "Sum: " << double (sum) << " " << "Average: " << double (sum/count) << endl;
+
+    cout << endl;
 
     // Loop 2
     int m2, n2;
@@ -26,5 +38,12 @@ int main() {
     cout << "Enter m and n:";
     cin >> m2 >> n2;
 
-    cout << "Expected: Integers from " << m2 << " to " << n2 << endl;
+    cout << "Expected: odd integers from " << m2 << " to " << n2 << endl;
+    cout << "Actual: ";
+
+    for (int i = m2; i < (n2 + 1); ++i) {
+        if (i % 2 != 0) {
+            cout << i << " ";
+        }
+    }
 }
