df14bb10d3d52d4d6b881eb22415b0c90710198e
Rako-BMac <patrick.rakowicz@gmail.com>
2022-02-11 02:31:01 -0700

    Fixed ToLower Function | ++

--- CS-191/Function_Warm_Up/main.cpp
+++ CS-191/Function_Warm_Up/main.cpp
@@ -48,19 +48,29 @@ void toLowerCase(string &s) {
     //modifies s so that it is all lowercase; this is a pass by reference
     if (!s.empty()) {
         transform(begin(s), end(s), begin(s), [](char const &c) { return tolower(c); });
-        s[0] = toupper(s[0]);
+        s[0] = tolower(s[0]);
     }
 }
 
-//int xToY(int x, int y) {
-//    //uses a loop to calculate x^y and return it; assume y >= 0
-//}
-//
-//bool isAllCaps(string s) {
-//    //return true if s is all capital ASCII letters, false otherwise; take a look
-//    //at asciitable.com
-//}
-//
+// TODO
+//  Work on factorial
+//  cant change int y to long double y
+/*int xToY(int x, long double y) {
+    //uses a loop to calculate x^y and return it; assume y >= 0
+    if (x < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else {
+        for(int i = 1; i <= x; ++i) {
+            y *= i;
+        }
+        return y;
+    }
+}*/
+
+bool isAllCaps(string s) {
+    //return true if s is all capital ASCII letters, false otherwise; take a look at asciitable.com
+}
+
 //void toProperCaps(string &s) {
 //    //modifies s so that the first character and any other characters which are
 //    //after a blank are capitalized, all other letters are lowercased. This is a
@@ -68,10 +78,13 @@ void toLowerCase(string &s) {
 //}
 
 int main() {
-    string a = "PATRICK";
+    string a = "TEST";
+    toLowerCase(a);
+
     cout << "Largest is: " << maxIntVal(4, 2) << endl;
     cout << "Sum count: " << sumN(10) << endl;
     cout << "Is N Even?: " << boolalpha << isEven(2) << endl;
-    cout << "Fixed Caps: " << fixCaps("pAtRiCk") << endl;
-    cout << "All Lower: " << toLowerCase(a) << endl;
+    cout << "Fixed Caps: " << fixCaps("tEST") << endl;
+    cout << "All Lower: " << a << endl;
+    //cout << "Factorial :" << xToY(12, 1) << endl;
 }
