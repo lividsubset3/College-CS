c974815eda4a5f0ea751082599921f5e6827c875
Patrick Rakowicz <lividsubset3@gmail.com>
2022-02-16 13:40:08 -0700

    Finished | Debugged

--- CS-195/ConInfo_1_to_N_Joins.sql
+++ CS-195/ConInfo_1_to_N_Joins.sql
@@ -13,3 +13,13 @@ on friend.friendId = phones.friendId
 where friendName = 'Mike Lopez';
 
 #3
+select email as "Email", friendName as "Name"
+from friends join emails
+on emails.friendId = friends.friendId
+order by friendName;
+
+#4
+select phone as "Phone Number", friendName as "Name"
+from phones join friends
+on phones.friendId = friends.friendId
+order by friendName;
