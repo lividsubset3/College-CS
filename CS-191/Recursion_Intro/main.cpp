201a76863befc158fc5e0c42d46aef9d8a49dd1e
Patrick Rakowicz <lividsubset3@gmail.com>
2022-03-08 14:37:35 -0700

    Recursion Intro Init

---
+++ CS-191/Recursion_Intro/main.cpp
@@ -0,0 +1,53 @@
+/*
+    191
+    Recursion Intro
+    main.cpp
+    Patrick Rakowicz
+*/
+#include <iostream>
+
+using namespace std;
+
+
+void hailstone(int n) {
+
+}
+
+int fibonacci(int n) {
+
+}
+
+int sumDigits(int n) {
+
+}
+
+int sum(int n) {
+
+}
+
+bool isPalindrome(string s) {
+
+}
+
+int main() {
+    cout << "hailstone(50)" << endl;
+    hailstone(50);
+    cout << endl;
+    cout << "fibonacci(10)" << endl;
+    cout << fibonacci(10) << endl << endl;
+    cout << "sumDigits(88497)" << endl;
+    cout << sumDigits(88497) << endl << endl;
+    cout << "sum(10)" << endl;
+    cout << sum(10) << endl;
+    cout << "sum(1)" << endl;
+    cout << sum(1) << endl;
+    cout << boolalpha;
+    cout << "isPalindrome(\"mom\")" << endl;
+    cout << isPalindrome("mom") << endl;
+    cout << "isPalindrome(\"momxmom\")" << endl;
+    cout << isPalindrome("momxmom") << endl;
+    cout << "isPalindrome(\"baker\")" << endl;
+    cout << isPalindrome("baker") << endl;
+    cout << "isPalindrome(\"bakkaB\")" << endl;
+    cout << isPalindrome("bakkaB") << endl;
+}
