b411970b443a9da28badcb920fbc370b9248bbd4
Patrick Rakowicz <lividsubset3@gmail.com>
2022-03-11 21:46:45 -0700

    Finished all recursion | Debugged

--- CS-191/Recursion_Intro/main.cpp
+++ CS-191/Recursion_Intro/main.cpp
@@ -2,10 +2,9 @@
     191
     Recursion Intro
     main.cpp
-    Patrick Rakowicz
+    Patrick Rakowicz | Caleb Domenico
 */
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 
@@ -19,11 +18,10 @@ void hailstone(int n) {
     if (n == 1) {
         abort();
     } else if (n % 2 == 0) {
-        cout << n/2 << endl;
+        cout << n / 2 << endl;
         cout << "hailstone(" << n << "/2)";
     } else {
         cout << 3 * n + 1 << " " << "hailstone(3*" << n << "+1)" << endl;
-        //return;
     }
 }
 
@@ -35,7 +33,7 @@ int fibonacci(int n) {
     if (n == 1 || n == 0) {
         return 1;
     } else {
-        return (n-1) + (n-2);
+        return (n - 1) + (n - 2);
     }
 }
 
@@ -43,44 +41,57 @@ int fibonacci(int n) {
     int sumDigits(int n) - return the sum of the digits of an integer n
 */
 int sumDigits(int n) {
-
+    int sum = 0;
+    while (n != 0) {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+    return sum;
 }
 
 /*
     int sum(int n) - return the sum of the integers from 1 to n
 */
 int sum(int n) {
-
+    int sum = 0;
+    while (n > 0) {
+        sum += n;
+        n--;
+    }
+    return sum;
 }
 
 /*
-    bool isPalindrome(string s)) - test the string for reading the same
+    bool isPalindrome(string s) - test the string for reading the same
     front to back and back to front;
     ignore case in the comparisons.
 */
 bool isPalindrome(string s) {
-
+    if (equal(s.begin(), s.begin() + s.size()/2, s.rbegin()))
+        return true;
+    else
+        return false;
 }
 
 int main() {
     cout << "hailstone(50)" << endl;
     hailstone(50);
-//    cout << endl;
-//    cout << "fibonacci(10)" << endl;
-//    cout << fibonacci(10) << endl << endl;
-//    cout << "sumDigits(88497)" << endl;
-//    cout << sumDigits(88497) << endl << endl;
-//    cout << "sum(10)" << endl;
-//    cout << sum(10) << endl;
-//    cout << "sum(1)" << endl;
-//    cout << sum(1) << endl;
-//    cout << boolalpha;
-//    cout << "isPalindrome(\"mom\")" << endl;
-//    cout << isPalindrome("mom") << endl;
-//    cout << "isPalindrome(\"momxmom\")" << endl;
-//    cout << isPalindrome("momxmom") << endl;
-//    cout << "isPalindrome(\"baker\")" << endl;
-//    cout << isPalindrome("baker") << endl;
-//    cout << "isPalindrome(\"bakkaB\")" << endl;
-//    cout << isPalindrome("bakkaB") << endl;
+    cout << endl;
+    cout << "fibonacci(10)" << endl;
+    cout << fibonacci(10) << endl;
+    cout << "sumDigits(88497)" << endl;
+    cout << sumDigits(88497) << endl;
+    cout << "sum(10)" << endl;
+    cout << sum(10) << endl;
+    cout << "sum(1)" << endl;
+    cout << sum(2) << endl;
+    cout << boolalpha;
+    cout << "isPalindrome(\"mom\")" << endl;
+    cout << isPalindrome("mom") << endl;
+    cout << "isPalindrome(\"momxmom\")" << endl;
+    cout << isPalindrome("momxmom") << endl;
+    cout << "isPalindrome(\"baker\")" << endl;
+    cout << isPalindrome("baker") << endl;
+    cout << "isPalindrome(\"bakkaB\")" << endl;
+    cout << isPalindrome("bakkaB") << endl;
 }
