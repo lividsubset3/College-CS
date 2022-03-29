42019d524dfc6e0e45cb4453e74aa7577db4a2f7
Pat Rak <rako@Pats-MacBook-Pro.local>
2022-03-29 17:37:29 -0600

    Problem one done

--- CS-195/Modifications_Filters_Functions.sql
+++ CS-195/Modifications_Filters_Functions.sql
@@ -1,4 +1,28 @@
+/*
+	Patrick Rakowicz
+    CS 195
+    Modifications Filters Functions
+*/
 use customersAndProducts;
 
 #1
+select fname, lname
+from customerContacts;
+#1.a
+select fname, lname
+from customerContacts
+where fname like 'A%';
+#1.b
+select fname, lname
+from customerContacts
+where fname like 'A%' and lname like 'L%';
+#1.c
+select fname, lname
+from customerContacts
+where fname between 'D%' and 'M%';
+#1.d
+select fname, lname
+from customerContacts
+where fname like binary 'A%';
 
+#2
