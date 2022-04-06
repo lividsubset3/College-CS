9ef74865b82d68003c3e56365cac8fc37954cec5
Patrick Rakowicz <lividsubset3@gmail.com>
2022-04-06 15:34:50 -0600

    MFF Assignment (#15)
    * File Init | Start
    
    * Working on #1
    
    * Problem one done
    
    * Added #7
    
    * Finished 2
    
    * 4 Questions not done / broken
    
    * Small Changes
    
    * Missing 3 | else done
    
    * Finished 6
    
    * Debug 6 | Slightly Broken
    
    * Finished 6 | 3 still broke
    
    * 3 works kinda | Not sure how to do
    
    * Changed file header
    
    Co-authored-by: Pat Rak <rako@Pats-MacBook-Pro.local>

---
+++ CS-195/Modifications_Filters_Functions.sql
@@ -0,0 +1,101 @@
+/* 
+   CS 195
+   Modifications Filters Functions
+   Modifications_Filter_Functions.sql
+   Patrick Rakowicz
+*/
+use customersAndProducts;
+
+#1.a
+select fname, lname
+from customerContacts;
+#1.b
+select fname, lname
+from customerContacts
+where fname like 'A%';
+#1.c
+select fname, lname
+from customerContacts
+where fname like 'A%' and lname like 'L%';
+#1.d
+select fname, lname
+from customerContacts
+where fname between 'D%' and 'M%';
+#1.e
+select fname, lname
+from customerContacts
+where fname like binary 'A%' and fname like binary '%a%';
+
+#2
+select distinct email
+from customerContacts
+where email not like '%.com%';
+
+#3
+select distinct productId, quantity,
+if((select sum(quantity) from orderLines where quantity <= 5) <= 5, 1.00, null) 'Shipping Cost'
+from orderLines;
+
+select distinct productId, quantity,
+if(5 < (select sum(quantity) from orderLines) <= 10, 3.00, null) 'Shipping Cost'
+from orderLines;
+
+select distinct productId, quantity,
+if((select sum(quantity) from orderLines) < 10, 5.00, null) 'Shipping Cost'
+from orderLines;
+
+#4
+select email
+from customerContacts
+where email regexp 'hawk|hawkins|hawking';
+
+#5
+select lname
+from customerContacts
+where lname like 'T%' and char_length(lname) <= 8;
+
+#6
+select concat(upper(left(lname, 1)), 
+	lower(substring(lname, 2, length(lname) - 2)),
+	upper(right(lname, 1))) as 'Cap first/last letter'
+from customerContacts;
+
+#7
+SELECT date_format('2011-3-7', '%W, %M %e, %Y') AS result;
+select date_format('2011-3-7', '%M %D, %Y') as result;
+select date_format('2011-3-7 16:05:00', '%W %l:%i %p') as result;
+
+#8
+select date_format(date_add(curdate(), interval 1 month), '%m/%d/%y') as result;
+select date_format(date_add(curdate(), interval 10 week), '%m/%d/%y') as result;
+select date_format(date_add(curdate(), interval 40 day), '%m/%d/%y') as result;
+select date_format(date_sub(curdate(), interval 40 day), '%m/%d/%y') as result;
+
+#9
+select date_format(addtime(curdate(), '100:00:00'), '%I:%i %p') as result;
+select date_format(subtime(curdate(), '100:00:00'), '%I:%i %p') as result;
+
+#10
+select customerId, count(fname) from customerContacts
+group by customerId;
+
+select * from customerContacts;
+
+#11
+select orderDate, quantity from orderlines
+join orders
+on orderlines.orderId = orders.orderId
+group by orderDate
+order by orderDate asc;
+
+#12
+select customerId, min(orderDate) from orders
+join orderLines
+on orderlines.orderId = orders.orderId
+group by customerId;
+
+#13
+select orderDate, quantity from orderlines
+join orders
+on orderlines.orderId = orders.orderId
+where quantity > 1;
