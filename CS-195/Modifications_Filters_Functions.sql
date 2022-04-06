2b98db897abc0b534e91cb662f1a2dd5f06db48e
Patrick Rakowicz <lividsubset3@gmail.com>
2022-04-05 20:01:32 -0600

    4 Questions not done / broken

--- CS-195/Modifications_Filters_Functions.sql
+++ CS-195/Modifications_Filters_Functions.sql
@@ -5,33 +5,62 @@
 */
 use customersAndProducts;
 
-#1
-select fname, lname
-from customerContacts;
 #1.a
 select fname, lname
-from customerContacts
-where fname like 'A%';
+from customerContacts;
 #1.b
 select fname, lname
 from customerContacts
-where fname like 'A%' and lname like 'L%';
+where fname like 'A%';
 #1.c
 select fname, lname
 from customerContacts
-where fname between 'D%' and 'M%';
+where fname like 'A%' and lname like 'L%';
 #1.d
 select fname, lname
 from customerContacts
-where fname like binary 'A%';
+where fname between 'D%' and 'M%';
+#1.e
+select fname, lname
+from customerContacts
+where fname like binary 'A%' and fname like binary '%a%';
 
 #2
 select distinct email
 from customerContacts
 where email not like '%.com%';
 
-#7
-select curdate();
-SELECT DATE_FORMAT('2011-3-7', '%a / %b') AS Result;
+#3 Not Finished
+select quantity, cost
+from orderLines join products
+on orderLines.productId = products.productId;
 
-# char_length(lname) < 8
+#4
+select email
+from customerContacts
+where email regexp 'hawk|hawkins|hawking';
+
+#5
+select lname
+from customerContacts
+where lname like 'T%' and char_length(lname) <= 8;
+
+#6 Not Finished
+select lname
+from customerContacts
+order by customerContactId desc limit 1;
+
+#7
+SELECT date_format('2011-3-7', '%W, %M %e, %Y') AS result;
+select date_format('2011-3-7', '%M %D, %Y') as result;
+select date_format('2011-3-7 16:05:00', '%W %l:%i %p') as result;
+
+#8
+select date_format(date_add(curdate(), interval 1 month), '%m/%d/%y') as result;
+select date_format(date_add(curdate(), interval 1 week), '%m/%d/%y') as result;
+select date_format(date_add(curdate(), interval 40 day), '%m/%d/%y') as result;
+select date_format(date_sub(curdate(), interval 40 day), '%m/%d/%y') as result;
+
+#9
+select date_format(addtime(curdate(), '100:00:00'), '%I:%i %p') as result;
+select date_format(subtime(curdate(), '100:00:00'), '%I:%i %p') as result;
